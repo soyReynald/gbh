e201598642b9ebb768c36b8381efb3a1fc9685c6
Reynald Ramirez <reynaldramirez@hotmail.com>
2022-02-20 22:36:20 -0400

    [U] Little update to complete the test

--- problem.js
+++ problem.js
@@ -1,18 +1,21 @@
 const testVehicles = [
-    { conductor: "Juan", speed: 900, velocityType: "km/h" },
+    { conductor: "Juan", speed: 3000, velocityType: "km/h" },
     { conductor: "Reynald", speed: 300, velocityType: "km/h" },
-    { conductor: "Jose", speed: 250, velocityType: "km/h" },
+    { conductor: "Jose", speed: 2000, velocityType: "mph" },
 ];
 
 const program = (vehicles) => {
     let firstVehicle = {};
     firstVehicle.speed = 0;
     vehicles.map((vehicle) => {
+        if (vehicle.type === "mph") {
+            vehicle.speed = vehicle.speed * 0.621371;
+        }
         if (vehicle.speed > firstVehicle?.speed) {
             firstVehicle = vehicle;
         }
     });
-    return `The fastest car is ${firstVehicle.conductor}'s`;
+    return `The fastest car is ${firstVehicle.conductor}'s and his velocity is: ${firstVehicle.speed} ${firstVehicle.velocityType}`;
 };
 
 console.log(program(testVehicles));
